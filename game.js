37d33a6ea47b628b6ea443e3ded4065e1ce11d26
issei11 <isseim1129@g.ecc.u-tokyo.ac.jp>
2024-12-17 01:37:08 +0900

    first commit

---
+++ game.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas === null || canvas === void 0 ? void 0 : canvas.getContext("2d");
+ctx.fillStyle = "red";
+ctx.fillRect(0, 0, 100, 100);
