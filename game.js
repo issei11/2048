6560178f19364822d2a73cc7988d1fe91b2e5e20
issei11 <isseim1129@g.ecc.u-tokyo.ac.jp>
2024-12-18 23:22:07 +0900

    4 * 4

--- game.js
+++ game.js
@@ -1,4 +1,25 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas === null || canvas === void 0 ? void 0 : canvas.getContext("2d");
-ctx.fillStyle = "red";
-ctx.fillRect(0, 0, 100, 100);
+var base = document.getElementById("base");
+var baseContext = base === null || base === void 0 ? void 0 : base.getContext("2d");
+if (!baseContext) {
+    console.error("Canvas not found");
+}
+else {
+    var rows = 4;
+    var cols = 4;
+    var margin = 10;
+    var totalMarginWidth = (cols + 1) * margin;
+    var totalMarginHeight = (rows + 1) * margin;
+    var baseWidth = base === null || base === void 0 ? void 0 : base.width;
+    var baseHeight = base === null || base === void 0 ? void 0 : base.height;
+    var cellWidth = (baseWidth - totalMarginWidth) / cols;
+    var cellHeight = (baseHeight - totalMarginHeight) / rows;
+    baseContext.strokeStyle = "black";
+    baseContext.strokeRect(0, 0, baseWidth, baseHeight);
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            var x = margin + j * (cellWidth + margin);
+            var y = margin + i * (cellHeight + margin);
+            baseContext.strokeRect(x, y, cellWidth, cellHeight);
+        }
+    }
+}
--- game.ts
+++ game.ts
@@ -1,5 +1,28 @@
-const canvas = document.getElementById("canvas") as HTMLCanvasElement | null;
-const ctx = canvas?.getContext("2d");
+const base = document.getElementById("base") as HTMLCanvasElement | null;
+const baseContext = base?.getContext("2d");
 
-ctx.fillStyle = "red";
-ctx.fillRect(0, 0, 100, 100);
+if (!baseContext) {
+    console.error("Canvas not found");
+}
+else {
+    const rows: number = 4;
+    const cols: number = 4;
+    const margin: number = 10;
+    const totalMarginWidth: number = (cols + 1) * margin;
+    const totalMarginHeight: number = (rows + 1) * margin;
+    const baseWidth: number = base?.width!;
+    const baseHeight: number = base?.height!;
+    const cellWidth: number = (baseWidth - totalMarginWidth) / cols;
+    const cellHeight: number = (baseHeight - totalMarginHeight) / rows;
+
+    baseContext.strokeStyle = "black";
+    baseContext.strokeRect(0, 0, baseWidth, baseHeight);
+
+    for (let i: number = 0; i < rows; i++) {
+        for (let j: number = 0; j < cols; j++) {
+            const x = margin + j * (cellWidth + margin);
+            const y = margin + i * (cellHeight + margin);
+            baseContext.strokeRect(x, y, cellWidth, cellHeight);
+        }
+    }
+}
