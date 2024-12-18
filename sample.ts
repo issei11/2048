471f723e5b1948ec94d2479aca2c105371adffdc
issei11 <isseim1129@g.ecc.u-tokyo.ac.jp>
2024-12-18 22:23:29 +0900

    first commit

---
+++ sample.ts
@@ -0,0 +1,5 @@
+const canvas = document.querySelector('.canvas'); // Canvasの取得
+const context = canvas.getContext('2d'); // Canvasからコンテキスを取得
+
+context.fillStyle = 'green'; // 描画の塗り色を決める
+context.fillRect(10, 10, 100, 100); // 位置とサイズを決めて描画
